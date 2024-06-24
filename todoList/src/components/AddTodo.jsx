7553b33c02ab93c4f735d5b841561df283ce02b9
superashishb <bhandariashish2022@gmail.com>
2024-06-24 12:07:11 +0530

    adding onchange , onSubmit useState in addtodo.jsx

--- todoList/src/components/AddTodo.jsx
+++ todoList/src/components/AddTodo.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Button, TextInput } from "flowbite-react";
+
 export default function AddTodo() {
+  const [input, setInput] = useState("");
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
   return (
-    <form className="flex flex-row justify-between items-center gap-5 bg-black p-4 rounded w-1/3 mt-6">
-      <TextInput id="email1" type="email" placeholder="Add Todo Item" required className="w-4/5" />
+    <form onSubmit={addTodoHandler} className="flex flex-row justify-between items-center gap-5 bg-black p-4 rounded w-1/3 mt-6">
+      <TextInput id="email1" type="text" placeholder="Add Todo Item" required className="w-4/5" value={input} onChange={(e) => setInput(e.target.value)}  />
   
-      <Button gradientDuoTone="purpleToBlue" className="w-1/5">Add Todo</Button>
+      <Button type="submit" gradientDuoTone="purpleToBlue" className="w-1/5">Add Todo</Button>
 
   </form>
 
