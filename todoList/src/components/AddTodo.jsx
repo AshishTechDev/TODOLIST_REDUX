8d67c5ccf41e0d05dab22cd61ec1d754330e5312
superashishb <bhandariashish2022@gmail.com>
2024-06-24 12:14:43 +0530

    usedispatch uses reducers to send data to store

--- todoList/src/components/AddTodo.jsx
+++ todoList/src/components/AddTodo.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { Button, TextInput } from "flowbite-react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
 
 export default function AddTodo() {
   const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
   const addTodoHandler = (e) => {
     e.preventDefault();
-    console.log(input);
+    dispatch(addTodo(input));
+    setInput("");
   }
   return (
     <form onSubmit={addTodoHandler} className="flex flex-row justify-between items-center gap-5 bg-black p-4 rounded w-1/3 mt-6">
